43122d14b5e7090136ce4e507f2a621e69cccd9c
Vladimir Dudko <v.dudko126@gmail.com>
2022-06-05 21:18:00 +0300

    добавляет функцию рандомного числа и проверку строки

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// random number
+function randomInteger(min, max) {
+  const random = min + Math.random() * (max + 1 - min);
+  return Math.floor(random);
+}
+window.console.log( randomInteger(0, 544));
+
+// Тернарник
+// const testString = 140;
+// let clearString = (testString <= 140) ? 'true' : 'false';
+// window.console.log(clearString);
+
+const testString = 140;
+let clearString;
+if (testString <= 140) {
+  clearString = true;
+} else {
+  clearString = false;
+}
+
+window.console.log(clearString);
