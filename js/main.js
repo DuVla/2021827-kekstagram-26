7b10d7e24d67eecb40684c7b07de65a03a8e2bb3
Vladimir Dudko <v.dudko126@gmail.com>
2022-06-16 23:59:55 +0300

    Добавляю дополнительные функции

--- js/main.js
+++ js/main.js
@@ -1,100 +1,219 @@
-/* eslint-disable no-unexpected-multiline */
-// random number
-function getRandomInteger(min, max) {
-  if (min < 0 || max < 0) {
-    throw new RangeError('Числа не должны быть отрицательными');
-  }
-  const random = min + Math.random() * (max + 1 - min);
-  return Math.floor(random);
-}
-window.console.log( getRandomInteger(1, 10));
-
-
 // String
 function checkStringLength(str, maxLength) {
   return str.length <= maxLength;
 }
 window.console.log(checkStringLength('text', 140));
 
+// random number
+function getRandomPositiveInteger(min, max) {
+  if (min < 0 || max < 0) {
+    throw new RangeError('Числа не должны быть отрицательными');
+  }
+  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
+  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
+  const result = Math.random() * (upper - lower + 1) + lower;
 
-const IDS = [
-  '1',
-  '2',
-  '3',
-  '4',
-  '5',
-  '6',
-  '7',
-  '8',
-  '9',
-  '10',
-  '11',
-  '12',
-  '13',
-  '14',
-  '15',
-  '16',
-  '17',
-  '18',
-  '19',
-  '20',
-  '21',
-  '22',
-  '23',
-  '24',
-  '25'
-];
+  return Math.floor(result);
+}
+window.console.log(getRandomPositiveInteger(1, 20));
 
-const URLS = [
-  'photos/.1.jpg',
-  'photos/.2.jpg',
-  'photos/.3.jpg',
-  'photos/.4.jpg',
-  'photos/.5.jpg',
-  'photos/.6.jpg',
-  'photos/.7.jpg',
-  'photos/.8.jpg',
-  'photos/.9.jpg',
-  'photos/.10.jpg',
-  'photos/.11.jpg',
-  'photos/.12.jpg',
-  'photos/.13.jpg',
-  'photos/.14.jpg',
-  'photos/.15.jpg',
-  'photos/.16.jpg',
-  'photos/.17.jpg',
-  'photos/.18.jpg',
-  'photos/.19.jpg',
-  'photos/.20.jpg',
-  'photos/.21.jpg',
-  'photos/.22.jpg',
-  'photos/.23.jpg',
-  'photos/.24.jpg',
-  'photos/.25.jpg',
-];
+// const LIKES = [
+//   '15',
+//   '16',
+//   '17',
+//   '18',
+//   '19',
+//   '20',
+//   '21',
+//   '22',
+//   '23',
+//   '24',
+//   '25',
+//   '26',
+//   '27',
+//   '28',
+//   '29',
+//   '30',
+//   '31',
+//   '32',
+//   '33',
+//   '34',
+//   '35',
+//   '36',
+//   '37',
+//   '38',
+//   '39',
+//   '40',
+//   '41',
+//   '42',
+//   '43',
+//   '44',
+//   '45',
+//   '46',
+//   '47',
+//   '48',
+//   '49',
+//   '50',
+//   '51',
+//   '52',
+//   '53',
+//   '54',
+//   '55',
+//   '56',
+//   '57',
+//   '58',
+//   '59',
+//   '60',
+//   '61',
+//   '62',
+//   '63',
+//   '64',
+//   '65',
+//   '66',
+//   '67',
+//   '68',
+//   '69',
+//   '70',
+//   '71',
+//   '72',
+//   '73',
+//   '74',
+//   '75',
+//   '76',
+//   '77',
+//   '78',
+//   '79',
+//   '80',
+//   '81',
+//   '82',
+//   '83',
+//   '84',
+//   '85',
+//   '86',
+//   '87',
+//   '88',
+//   '89',
+//   '90',
+//   '91',
+//   '92',
+//   '93',
+//   '94',
+//   '95',
+//   '96',
+//   '97',
+//   '98',
+//   '99',
+//   '100',
+//   '101',
+//   '102',
+//   '103',
+//   '104',
+//   '105',
+//   '106',
+//   '107',
+//   '108',
+//   '109',
+//   '110',
+//   '111',
+//   '112',
+//   '113',
+//   '114',
+//   '115',
+//   '116',
+//   '117',
+//   '118',
+//   '119',
+//   '120',
+//   '121',
+//   '122',
+//   '123',
+//   '124',
+//   '125',
+//   '126',
+//   '127',
+//   '128',
+//   '129',
+//   '130',
+//   '131',
+//   '132',
+//   '133',
+//   '134',
+//   '135',
+//   '136',
+//   '137',
+//   '138',
+//   '139',
+//   '140',
+//   '141',
+//   '142',
+//   '143',
+//   '144',
+//   '145',
+//   '146',
+//   '147',
+//   '148',
+//   '149',
+//   '150',
+//   '151',
+//   '152',
+//   '153',
+//   '154',
+//   '155',
+//   '156',
+//   '157',
+//   '158',
+//   '159',
+//   '160',
+//   '161',
+//   '162',
+//   '163',
+//   '164',
+//   '165',
+//   '166',
+//   '167',
+//   '168',
+//   '169',
+//   '170',
+//   '171',
+//   '172',
+//   '173',
+//   '174',
+//   '175',
+//   '176',
+//   '177',
+//   '178',
+//   '179',
+//   '180',
+//   '181',
+//   '182',
+//   '183',
+//   '184',
+//   '185',
+//   '186',
+//   '187',
+//   '188',
+//   '189',
+//   '190',
+//   '191',
+//   '192',
+//   '193',
+//   '194',
+//   '194',
+//   '196',
+//   '197',
+//   '198',
+//   '199',
+//   '200',
+// ];
 
 const DESCRIPTIONS = [
   'Передо мной интересная (удачная, занимательная и т.п.) фотография.',
 ];
 
-
-const LIKES = [
-  getRandomInteger(15, 200),
-];
-
 // Comments
 
 const ID_COMMENTS = [
-  getRandomInteger(1, 400),
-];
-
-const AVATARS = [
-  'img/avatar-1.svg',
-  'img/avatar-2.svg',
-  'img/avatar-3.svg',
-  'img/avatar-4.svg',
-  'img/avatar-5.svg',
-  'img/avatar-6.svg',
+  getRandomPositiveInteger(1, 400),
 ];
 
 const MESSAGES = [
@@ -132,21 +251,74 @@ const NAMES = [
 
 const SIMILAR_IMG_COUNT = 25;
 
-const getRandomArrayElement = (elements) => elements
-  [getRandomInteger(0, elements.length - 1)];
+const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];
 
-const createFoto = () => ({
-  id: getRandomArrayElement(IDS),
-  url: getRandomArrayElement(URLS),
-  description: getRandomArrayElement(DESCRIPTIONS),
-  likes: getRandomArrayElement(LIKES),
-  comments: {
+const createFoto = (id,) => ({
+  infoCards: [{
+    id: id,
+    url: `photos/.${id}.jpg`,
+    description: getRandomArrayElement(DESCRIPTIONS),
+    likes: getRandomPositiveInteger(15, 200),
+  }],
+  comments: [{
     id: getRandomArrayElement(ID_COMMENTS),
-    avatar: getRandomArrayElement(AVATARS),
+    avatar: `img/avatar-${getRandomPositiveInteger(1, 6)}.svg`,
     message: getRandomArrayElement(MESSAGES),
     name: getRandomArrayElement(NAMES),
-  }
+  }],
+  createComments: [{
+    id: getRandomArrayElement(ID_COMMENTS),
+    avatar: `img/avatar-${getRandomPositiveInteger(1, 6)}.svg`,
+    message: getRandomArrayElement(MESSAGES),
+    name: getRandomArrayElement(NAMES),
+  }],
 });
 
-const similarPhotos = Array.from({length: SIMILAR_IMG_COUNT}, createFoto);
-window.console.log(similarPhotos);
+// const similarPhotos = Array.from({ length: SIMILAR_IMG_COUNT }, createFoto,);
+// console.log(similarPhotos);
+// id
+const makePhotos = () => {
+  const IDS = [];
+  for (let i = 1; i <= 25; i++) {
+    IDS.push(createFoto(i));
+  }
+  return IDS;
+};
+
+const IDS = makePhotos(SIMILAR_IMG_COUNT);
+window.console.log(IDS);
+
+// const makeUrls = () => {
+//   const URLS = [
+//     'photos/.1.jpg',
+//     'photos/.2.jpg',
+//     'photos/.3.jpg',
+//     'photos/.4.jpg',
+//     'photos/.5.jpg',
+//     'photos/.6.jpg',
+//     'photos/.7.jpg',
+//     'photos/.8.jpg',
+//     'photos/.9.jpg',
+//     'photos/.10.jpg',
+//     'photos/.11.jpg',
+//     'photos/.12.jpg',
+//     'photos/.13.jpg',
+//     'photos/.14.jpg',
+//     'photos/.15.jpg',
+//     'photos/.16.jpg',
+//     'photos/.17.jpg',
+//     'photos/.18.jpg',
+//     'photos/.19.jpg',
+//     'photos/.20.jpg',
+//     'photos/.21.jpg',
+//     'photos/.22.jpg',
+//     'photos/.23.jpg',
+//     'photos/.24.jpg',
+//     'photos/.25.jpg',
+//   ];
+//   for (let i = 0; i <= 25; i++) {
+//     URLS.push(createFoto(i));
+//   }
+//   return URLS;
+// };
+
