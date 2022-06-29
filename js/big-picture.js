2bed759fe2612b4b0ff60a68da9d1acd94c7928c
Vladimir Dudko <v.dudko126@gmail.com>
2022-06-29 20:23:45 +0300

    errors

---
+++ js/big-picture.js
@@ -0,0 +1,14 @@
+// модуль, который отвечаeт за отрисовку окна с полноразмерным изображением.
+import { createPhotos } from './data.js';
+
+const userBigPicture = document.querySelector('.big-picture');
+// userBigPicture.classList.remove('hidden');
+
+const bigPictureElement = userBigPicture.querySelector('.big-picture');
+const bigPictureTemplate = document.querySelector('.big-picture').content;
+const bigPicture = createPhotos;
+const bigPictureFragment = document.createDocumentFragment();
+bigPicture.forEach(({})=> {
+  const pictureElement = bigPictureTemplate.cloneNode(true);
+  pictureElement.querySelector('.big-picture__img').src = url;
+});
